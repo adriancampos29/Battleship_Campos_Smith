6fc351b1d7e76f4f11d3c28af363333ea9a5c69d
Sam Smith <samlsmith424@gmail.com>
2021-12-07 15:58:18 -0800

    Feat: add fired upon methods

--- lib/cell.rb
+++ lib/cell.rb
@@ -3,6 +3,7 @@ class Cell
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
   end
 
   def empty?
@@ -14,9 +15,15 @@ class Cell
   end
 
   def place_ship(ship)
-    @ship = ship 
+    @ship = ship
   end
 
+  def fired_upon?
+    @fired_upon
+  end
 
-
+  def fire_upon
+    ship.hit
+    @fired_upon = true
+  end
 end
