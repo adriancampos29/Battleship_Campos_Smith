c615d0295f1d8e729f4aaaf9ce28bacbadcf0100
Sam Smith <samlsmith424@gmail.com>
2021-12-07 15:57:56 -0800

    Test: fired up, hitting ship methods

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -31,4 +31,14 @@ RSpec.describe Cell do
     cell.place_ship(cruiser)
     expect(cell.empty?).to eq(false)
   end
+
+  it 'can be fired upon' do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    expect(cell.fired_upon?).to eq(false)
+    cell.fire_upon
+    expect(cell.ship.health).to eq(2)
+    expect(cell.fired_upon?).to eq(true)
+  end
 end
