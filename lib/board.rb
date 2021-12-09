c50df2f04284b3e6ddf3d0fae7e822f350a05188
Sam Smith <samlsmith424@gmail.com>
2021-12-08 20:50:08 -0800

    Feat: make board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,11 @@
 class Board
+  # hash = Hash.new(".")
+  # require "pry"; binding.pry
+  # hash["A1"]
+
+  new_hash = Hash.new {|hash,key| hash[key] = "."}
+  new_hash["A1"]
+  new_hash["A2"]
+  new_hash["A3"]
+  p new_hash
 end
