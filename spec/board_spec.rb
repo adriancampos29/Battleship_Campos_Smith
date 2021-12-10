12d9a132df245d37e69f08a12da188facd3d7b84
Adrian Campos <adriancampos2992@yahoo.com>
2021-12-09 17:43:18 -0800

    Test: create it knows which coordinates are valid

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe Board do
     board = Board.new
     expect(board.cells).to be_a(Hash)
   end
+
+  it 'knows which coordinates are valid' do
+    board = Board.new
+    board.cells
+    expect(board.valid_coordinate?("A1")).to eq(true)
+    expect(board.valid_coordinate?("D4")).to eq(true)
+    expect(board.valid_coordinate?("A5")).to eq(false)
+    expect(board.valid_coordinate?("E1")).to eq(false)
+    expect(board.valid_coordinate?("A22")).to eq(false)
+  end
 end
