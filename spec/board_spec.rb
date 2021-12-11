a7aac59a63ecb24d8300d22917b7ffd7f9a7c622
Adrian Campos <adriancampos2992@yahoo.com>
2021-12-11 14:08:23 -0800

    Test: can determine ship length is equal to placement length & placement is sequential

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -23,13 +23,21 @@ RSpec.describe Board do
     expect(board.valid_coordinate?("A22")).to eq(false)
   end
 
-  it 'can determine a valid placement' do
+  it 'can determine ship length is the same as placement input length' do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
 
     expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
     expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+  end
+
+  it 'can determine placement is sequential' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
     expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
   end
 end
