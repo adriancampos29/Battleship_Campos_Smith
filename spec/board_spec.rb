82c7465e3c53cc7189f6440f810d0d59e33ebe2b
Adrian Campos <adriancampos2992@yahoo.com>
2021-12-11 15:11:58 -0800

    Test: pass horiztonal, vertical, backwards tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -40,4 +40,22 @@ RSpec.describe Board do
     expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
     expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
   end
+
+  it 'cannot be placed backwards' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+  end
+
+  it 'cannot be diagonal' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+  end
 end
