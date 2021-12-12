9be7e69d2b317cb68cd6b308c969f1f5a877a42e
Sam Smith <samlsmith424@gmail.com>
2021-12-12 11:29:53 -0800

    Test: refactor tests for helpers

--- spec/placement_spec.rb
+++ spec/placement_spec.rb
@@ -5,27 +5,34 @@ require './lib/placement'
 
 RSpec.describe Placement do
 
-it 'can separate the coordinate keys into two arrays' do
+it 'exists' do
   board = Board.new
   placement = Placement.new
-  require "pry"; binding.pry
-  expect(placement.letters).to eq([])
-  expect(placement.nums).to eq([])
-end
 
-xit 'can fill the letters and nums arrays' do
-  board = Board.new
-  # cruiser = Ship.new("Cruiser", 3)
-  # submarine = Ship.new("Submarine", 2)
-  placement = Placement.new
-  cell = Cell.new(placement)
-
-  placement.split_keys
-  expect(placement.letters).to eq(["A", "A", "A", "A", "B", "B", "B", "B", "C", "C", "C", "C", "D", "D", "D", "D"])
-  expect(placement.nums).to eq(["1", "2", "3", "4", "1", "2", "3", "4", "1", "2", "3", "4", "1", "2", "3", "4"])
+  expect(placement).to be_a(Placement)
 end
 
 
+
 end
+# xit 'can separate the coordinate keys into two arrays' do
+#   board = Board.new
+#   placement = Placement.new
+#
+#   expect(placement.letters).to eq([])
+#   expect(placement.nums).to eq([])
+# end
+#
+# xit 'can fill the letters and nums arrays' do
+#   board = Board.new
+#   # cruiser = Ship.new("Cruiser", 3)
+#   # submarine = Ship.new("Submarine", 2)
+#   placement = Placement.new
+#   cell = Cell.new(placement)
+#
+#   placement.split_keys
+#   expect(placement.letters).to eq(["A", "A", "A", "A", "B", "B", "B", "B", "C", "C", "C", "C", "D", "D", "D", "D"])
+#   expect(placement.nums).to eq(["1", "2", "3", "4", "1", "2", "3", "4", "1", "2", "3", "4", "1", "2", "3", "4"])
+# end
 
 #horizontal, vertical, consecutive, empty/can't stack ships
