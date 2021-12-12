413307b55b4aa69e53aea70f44b291e11bb5e3f5
Sam Smith <samlsmith424@gmail.com>
2021-12-12 14:54:30 -0800

    Test: it cannot be diagonal and can have valid placement

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Board do
     expect(board.valid_coordinate?("A22")).to eq(false)
   end
 
-  it 'can determine ship length is the same as placement input length' do
+  it 'can determine ship length is the same as input placement length' do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
@@ -39,11 +39,26 @@ RSpec.describe Board do
 
     expect(board.consec_nums(cruiser, ["B2", "B3", "B4"])).to eq(true)
     expect(board.consec_nums(cruiser, ["B1", "B3", "B4"])).to eq(false)
-    expect(board.consec_nums(submarine, ["C1", "C4"])).to eq(false)
+    expect(board.consec_nums(submarine, ["A1", "C1"])).to eq(false)
     expect(board.consec_nums(cruiser, ["A1", "A2", "A4"])).to eq(false)
+  end
 
-    # expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
-    # expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+  it 'cannot be backwards on the horizontal axis' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+  end
+
+  it 'cannot be diagonal' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
   end
 
   it 'can determine vertical placement numbers is consecutive/sequential' do
@@ -77,6 +92,15 @@ RSpec.describe Board do
     expect(board.same_letters(["B1", "B2", "B3"])).to eq(true)
     expect(board.same_letters(["B1", "C2", "D3"])).to eq(false)
     expect(board.same_letters(["C1", "A3"])).to eq(false)
+  end
+
+  it 'can say a ship is valid if it has valid coordinates' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
+    expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
   end
 end
 
