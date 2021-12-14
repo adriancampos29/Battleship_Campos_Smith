c8866c50bc8abb94d92322884b65a3012ad1e3d4
Sam Smith <samlsmith424@gmail.com>
2021-12-13 18:48:36 -0800

    Feat: add instances variables

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -2,7 +2,11 @@ require './lib/board'
 require './lib/cell'
 require './lib/ship'
 require './lib/game'
+require './lib/computer'
+require './lib/player'
+
 
 game = Game.new
 player = Player.new
+computer = Computer.new 
 game.greeting
