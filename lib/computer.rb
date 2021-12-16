72a9603e2953aa5ca6664b96225310770419a0e5
Sam Smith <samlsmith424@gmail.com>
2021-12-15 16:29:00 -0800

    Feat: refactor deploy methods

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,26 +1,34 @@
 require './lib/board'
+require './lib/game'
 
 class Computer
-  attr_reader :board, :cruiser, :submarine
 
-  def initialize
-    @computer_board = Board.new
-    @computer_ships = [Ship.new("Cruiser", 3), Ship.new("Submarine", 2)]
-  end
-
-  def random_coords(ship_length)
-    array = @computer_board.cells.keys
-    array.shuffle!.pop(ship_length)
+  def initialize(muffin)
+    @computer_board = muffin
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def computer_place_ships
-    @computer_ships.each do |ship|
-      coords = random_coords(ship.length)
-      if @computer_board.valid_placement?(ship, coords) == true
-        @computer_board.place(ship, coords)
-      end
+    deploy_cruiser
+    deploy_sub
+  end
+
+  def deploy_cruiser
+    coords = @computer_board.cells.keys
+    c_cruiser = []
+    until @computer_board.valid_placement?(@cruiser, c_cruiser) == true
+      c_cruiser = coords.sample(3)
     end
-    puts "I have laid out my ships on the grid."
-    # user_place_ships
+    @computer_board.place(@cruiser, c_cruiser)
+  end
+
+  def deploy_sub
+    coords = @computer_board.cells.keys
+    c_sub = []
+    until @computer_board.valid_placement?(@submarine, c_sub)
+      c_sub = coords.sample(2)
+    end
+    @computer_board.place(@submarine, c_sub)
   end
 end
